1afc55373ff188afc606f8dbeaba22aa09b571af
jlgraner <32852039+jlgraner@users.noreply.github.com>
2019-01-24 16:17:54 -0500

    Add descriptions of input in run_fsl_to_tsv.py

--- tsv_file_related/run_fsl_to_tsv.py
+++ tsv_file_related/run_fsl_to_tsv.py
@@ -4,35 +4,35 @@ import os
 
 this_env = os.environ
 
-sub = '[[SUBJECT ID]]'
-ses = '[[SESSION ID]]'
-task = '[[TASK NAME]]'
-run = '[[RUN NUMBER]]'
+sub = '[[SUBJECT ID]]' # The ID number you use for this study participant (e.g. 'MS001')
+ses = '[[SESSION ID]]' # A short identifier for this scan session (e.g. 'day1')
+task = '[[TASK NAME]]' # The short name of your fMRI task (e.g. 'stroop')
+run = '[[RUN NUMBER]]' # Two-digit Run number of the task within the scan session (e.g. '01')
 
 input_file_list = [
-                   '[[FSL TXT FILE 1]]',
-                   '[[FSL TXT FILE 2]]',
-                   '[[FSL TXT FILE 3]]',
-                   '[[FSL TXT FILE 4]]',
-                   '[[FSL TXT FILE 5]]',
+                   '[[FSL TXT FILE 1]]', #List of .txt files containing the FSL-formatted event information for task conditions (EVs)
+                   '[[FSL TXT FILE 2]]', # (e.g. 'MS001_day1_stroop_01_incongruent.txt')
+                   '[[FSL TXT FILE 3]]', # The number of .txt files in this list must match the number of conditions in the
+                   '[[FSL TXT FILE 4]]', # condition list below. The order of the .txt files must also correspond to the
+                   '[[FSL TXT FILE 5]]', # order of the conditions. All of these files must be in the same directory.
                    '[[FSL TXT FILE 6]]'
                    ]
 
 condition_list = [
-                  '[[FSL CONDITION NAME 1]]',
-                  '[[FSL CONDITION NAME 2]]',
-                  '[[FSL CONDITION NAME 3]]',
-                  '[[FSL CONDITION NAME 4]]',
-                  '[[FSL CONDITION NAME 5]]',
+                  '[[FSL CONDITION NAME 1]]', #List of short names of task conditions to be used in the output .tsv file
+                  '[[FSL CONDITION NAME 2]]', # (e.g. 'incongruent')
+                  '[[FSL CONDITION NAME 3]]', # The number of conditions in this list must match the number of .txt files
+                  '[[FSL CONDITION NAME 4]]', # in the file list above. The order of the conditions must also correspond to
+                  '[[FSL CONDITION NAME 5]]', # the order of the text files.
                   '[[FSL CONDITION NAME 6]]'
                   ]
 
-input_dir = '[[DIRECTORY CONTAINING THE FILES IN input_file_list]]'
+input_dir = '[[DIRECTORY CONTAINING THE FILES IN input_file_list]]' # (e.g. '/usr/home/data/mystudy/MS001/fsl_ev_files/')
 
-output_dir = '[[DIRECTORY WHERE YOU WANT THE TSV FILE TO BE WRITTEN]]'
+output_dir = '[[DIRECTORY WHERE YOU WANT THE TSV FILE TO BE WRITTEN]]' # (e.g. '/usr/home/data/mystudy/MS001/BIDS_tsv_file/'; MUST EXIST BEFORE RUNNING)
 
 
-
+#-----------Do not edit below this line------------
 print('Calling fsl2tsv for subject: {}'.format(sub))
 
 file_list = []
