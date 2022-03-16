e60806d2dec7784b1ab721ff6c3cf40b973398f3
John Graner <32852039+jlgraner@users.noreply.github.com>
2022-03-16 16:49:56 -0400

    Update run_tsv_to_fsl.py
    Removed square brackets from example for clarity.

--- tsv_file_related/run_tsv_to_fsl.py
+++ tsv_file_related/run_tsv_to_fsl.py
@@ -1,8 +1,8 @@
 import fsl_tsv_tools as ftt
 
-input_file = '[[YOUR_INPUT_TSV_FILE]]'
+input_file = 'YOUR_INPUT_TSV_FILE'
 
-output_dir = '[[YOUR_OUTPUT_DIRECTORY]]'
+output_dir = 'YOUR_OUTPUT_DIRECTORY'
 
 ftt.tsv2fsl(input_file, output_dir)
 
